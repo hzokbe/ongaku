3c6cfae4ec2d1f66efa21c3c43f0120b39fb7974
hzokbe <hzokbe@gmail.com>
2025-06-30 20:38:00 -0300

    feat(exception): add InvalidSongTitleException class

---
+++ src/main/java/com/hzokbe/ongaku/exception/song/InvalidSongTitleException.java
@@ -0,0 +1,7 @@
+package com.hzokbe.ongaku.exception.song;
+
+public class InvalidSongTitleException extends RuntimeException {
+    public InvalidSongTitleException(String message) {
+        super(message);
+    }
+}
