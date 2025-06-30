e398b6a8b85e7cd1b23ea42370acc03cc32e6f63
hzokbe <hzokbe@gmail.com>
2025-06-30 20:36:34 -0300

    feat(repository): add SongRepository interface

---
+++ src/main/java/com/hzokbe/ongaku/repository/song/SongRepository.java
@@ -0,0 +1,12 @@
+package com.hzokbe.ongaku.repository.song;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hzokbe.ongaku.model.song.Song;
+
+@Repository
+public interface SongRepository extends JpaRepository<Song, UUID> {
+}
