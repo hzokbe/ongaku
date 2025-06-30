6620976c4da0a79ea5ecf90bb0682c996860745f
hzokbe <hzokbe@gmail.com>
2025-06-30 20:33:46 -0300

    feat(model): add Song class

---
+++ src/main/java/com/hzokbe/ongaku/model/song/Song.java
@@ -0,0 +1,44 @@
+package com.hzokbe.ongaku.model.song;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "songs")
+public class Song {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    public Song() {
+    }
+
+    public Song(String title) {
+        this.title = title;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
