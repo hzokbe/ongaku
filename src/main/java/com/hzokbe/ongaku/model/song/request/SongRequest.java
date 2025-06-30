59836831844e847738bbe85bf2b9b08460f27c1c
hzokbe <hzokbe@gmail.com>
2025-06-30 20:34:54 -0300

    feat(model): add SongRequest class

---
+++ src/main/java/com/hzokbe/ongaku/model/song/request/SongRequest.java
@@ -0,0 +1,13 @@
+package com.hzokbe.ongaku.model.song.request;
+
+public class SongRequest {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
