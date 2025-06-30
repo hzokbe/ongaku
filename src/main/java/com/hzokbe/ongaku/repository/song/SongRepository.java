3b3482c070a5f91ca5a5ec895a15423aeb13ba61
hzokbe <hzokbe@gmail.com>
2025-06-30 20:37:00 -0300

    feat(repository): add findByTitle method

--- src/main/java/com/hzokbe/ongaku/repository/song/SongRepository.java
+++ src/main/java/com/hzokbe/ongaku/repository/song/SongRepository.java
@@ -1,5 +1,6 @@
 package com.hzokbe.ongaku.repository.song;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,6 @@ import com.hzokbe.ongaku.model.song.Song;
 @Repository
 public interface SongRepository extends JpaRepository<Song, UUID> {
     boolean existsByTitle(String title);
+
+    Optional<Song> findByTitle(String title);
 }
