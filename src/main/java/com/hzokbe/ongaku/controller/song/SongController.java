29c924c18822a3a53ce34c4d8a69bd87758e75df
hzokbe <hzokbe@gmail.com>
2025-07-01 15:24:36 -0300

    feat(controller): add deleteById method

--- src/main/java/com/hzokbe/ongaku/controller/song/SongController.java
+++ src/main/java/com/hzokbe/ongaku/controller/song/SongController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,5 +45,12 @@ public class SongController {
     @PutMapping("/{id}")
     public ResponseEntity<SongResponse> update(@PathVariable UUID id, @RequestBody SongRequest request) {
         return ResponseEntity.status(HttpStatus.OK).body(service.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable UUID id) {
+        service.deleteById(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
