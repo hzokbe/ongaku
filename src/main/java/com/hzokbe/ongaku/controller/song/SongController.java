d518601e7f28194803f07c22c029870fc4c79a1a
hzokbe <hzokbe@gmail.com>
2025-07-01 15:18:32 -0300

    feat(controller): add update method

--- src/main/java/com/hzokbe/ongaku/controller/song/SongController.java
+++ src/main/java/com/hzokbe/ongaku/controller/song/SongController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +39,10 @@ public class SongController {
     @GetMapping("/{id}")
     public ResponseEntity<SongResponse> findById(@PathVariable UUID id) {
         return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SongResponse> update(@PathVariable UUID id, @RequestBody SongRequest request) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.update(id, request));
     }
 }
