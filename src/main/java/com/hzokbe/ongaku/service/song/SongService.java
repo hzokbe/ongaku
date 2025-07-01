0a8ae2bfbf7d71dbf942bb93c5be7a0ef23ab576
hzokbe <hzokbe@gmail.com>
2025-07-01 14:50:09 -0300

    feat(service): add save method

--- src/main/java/com/hzokbe/ongaku/service/song/SongService.java
+++ src/main/java/com/hzokbe/ongaku/service/song/SongService.java
@@ -2,6 +2,11 @@ package com.hzokbe.ongaku.service.song;
 
 import org.springframework.stereotype.Service;
 
+import com.hzokbe.ongaku.exception.song.AlreadyRegisteredSongException;
+import com.hzokbe.ongaku.exception.song.InvalidSongTitleException;
+import com.hzokbe.ongaku.model.song.Song;
+import com.hzokbe.ongaku.model.song.request.SongRequest;
+import com.hzokbe.ongaku.model.song.response.SongResponse;
 import com.hzokbe.ongaku.repository.song.SongRepository;
 
 @Service
@@ -10,5 +15,29 @@ public class SongService {
 
     public SongService(SongRepository repository) {
         this.repository = repository;
+    }
+
+    public SongResponse save(SongRequest request) {
+        var title = request.getTitle();
+
+        if (title == null) {
+            throw new InvalidSongTitleException("song title cannot be null");
+        }
+
+        title = title.trim();
+
+        if (title.isBlank()) {
+            throw new InvalidSongTitleException("song title cannot be blank");
+        }
+
+        if (repository.existsByTitle(title)) {
+            throw new AlreadyRegisteredSongException("song already registered");
+        }
+
+        var song = new Song(title);
+
+        song = repository.save(song);
+
+        return new SongResponse(song.getId(), title);
     }
 }
