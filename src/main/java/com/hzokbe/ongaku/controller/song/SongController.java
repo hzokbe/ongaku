60c18562aaf9177d9ee98b965b1b6f38043b0738
hzokbe <hzokbe@gmail.com>
2025-07-01 15:09:38 -0300

    feat(controller): add findById method

--- src/main/java/com/hzokbe/ongaku/controller/song/SongController.java
+++ src/main/java/com/hzokbe/ongaku/controller/song/SongController.java
@@ -1,10 +1,12 @@
 package com.hzokbe.ongaku.controller.song;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +33,10 @@ public class SongController {
     @GetMapping
     public ResponseEntity<List<SongResponse>> findAll() {
         return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SongResponse> findById(@PathVariable UUID id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
     }
 }
