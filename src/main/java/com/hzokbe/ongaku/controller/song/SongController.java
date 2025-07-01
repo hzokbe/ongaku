486867b9bb9978537726b7e7fef00242ff94248c
hzokbe <hzokbe@gmail.com>
2025-07-01 15:04:59 -0300

    feat(controller): add findAll method

--- src/main/java/com/hzokbe/ongaku/controller/song/SongController.java
+++ src/main/java/com/hzokbe/ongaku/controller/song/SongController.java
@@ -1,7 +1,10 @@
 package com.hzokbe.ongaku.controller.song;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +26,10 @@ public class SongController {
     @PostMapping
     public ResponseEntity<SongResponse> save(@RequestBody SongRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SongResponse>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
     }
 }
