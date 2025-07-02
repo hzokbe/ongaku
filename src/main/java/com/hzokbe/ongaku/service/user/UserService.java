a7d36536a5c8b8744d7e4f28e64fa5acedce04a6
hzokbe <hzokbe@gmail.com>
2025-07-02 14:50:17 -0300

    feat(service): add PasswordEncoder injection

--- src/main/java/com/hzokbe/ongaku/service/user/UserService.java
+++ src/main/java/com/hzokbe/ongaku/service/user/UserService.java
@@ -1,5 +1,6 @@
 package com.hzokbe.ongaku.service.user;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.hzokbe.ongaku.repository.user.UserRepository;
@@ -8,7 +9,11 @@ import com.hzokbe.ongaku.repository.user.UserRepository;
 public class UserService {
     private final UserRepository repository;
 
-    public UserService(UserRepository repository) {
+    private final PasswordEncoder encoder;
+
+    public UserService(UserRepository repository, PasswordEncoder encoder) {
         this.repository = repository;
+
+        this.encoder = encoder;
     }
 }
