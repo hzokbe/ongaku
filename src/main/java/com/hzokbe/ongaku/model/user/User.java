8df3d0ee50771a987e74cd2e9ad7f577ede9c31b
hzokbe <hzokbe@gmail.com>
2025-07-02 14:32:10 -0300

    feat(model): add User class

---
+++ src/main/java/com/hzokbe/ongaku/model/user/User.java
@@ -0,0 +1,57 @@
+package com.hzokbe.ongaku.model.user;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    public User() {
+    }
+
+    public User(String username, String passwordHash) {
+        this.username = username;
+
+        this.passwordHash = passwordHash;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+}
