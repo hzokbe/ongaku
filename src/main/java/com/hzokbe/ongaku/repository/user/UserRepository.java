ca30f2f0dc2b89aef21585893237461194aa8cf8
hzokbe <hzokbe@gmail.com>
2025-07-02 14:34:57 -0300

    feat(repository): add UserRepository interface

---
+++ src/main/java/com/hzokbe/ongaku/repository/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.hzokbe.ongaku.repository.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hzokbe.ongaku.model.user.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
