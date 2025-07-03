eb19961cbf0913160416bd24745c49e83909c796
hzokbe <hzokbe@gmail.com>
2025-07-03 19:21:06 -0300

    feat(service): add UserRepository injection

--- src/main/java/com/hzokbe/ongaku/service/user/custom/CustomUserDetailsService.java
+++ src/main/java/com/hzokbe/ongaku/service/user/custom/CustomUserDetailsService.java
@@ -2,6 +2,13 @@ package com.hzokbe.ongaku.service.user.custom;
 
 import org.springframework.stereotype.Service;
 
+import com.hzokbe.ongaku.repository.user.UserRepository;
+
 @Service
 public class CustomUserDetailsService {
+    private final UserRepository repository;
+
+    public CustomUserDetailsService(UserRepository repository) {
+        this.repository = repository;
+    }
 }
