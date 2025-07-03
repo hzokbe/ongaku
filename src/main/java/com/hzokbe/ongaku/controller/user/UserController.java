fd3612de2f51dba4f79446b5a7feed4bec8aea3e
hzokbe <hzokbe@gmail.com>
2025-07-03 19:17:43 -0300

    feat(controller): add signIn method

--- src/main/java/com/hzokbe/ongaku/controller/user/UserController.java
+++ src/main/java/com/hzokbe/ongaku/controller/user/UserController.java
@@ -2,10 +2,12 @@ package com.hzokbe.ongaku.controller.user;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hzokbe.ongaku.model.jwt.response.JwtResponse;
 import com.hzokbe.ongaku.model.user.request.UserRequest;
 import com.hzokbe.ongaku.model.user.response.UserResponse;
 import com.hzokbe.ongaku.service.user.UserService;
@@ -21,5 +23,10 @@ public class UserController {
     @PostMapping("/sign-up")
     public ResponseEntity<UserResponse> signUp(@RequestBody UserRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.signUp(request));
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<JwtResponse> signIn(Authentication authentication) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.signIn(authentication));
     }
 }
