a83a0ce61844407601be0213083fd88b0a95a491
hzokbe <hzokbe@gmail.com>
2025-07-03 19:24:24 -0300

    feat(config): update getSecurityFilterChain method

--- src/main/java/com/hzokbe/ongaku/config/security/SecurityConfig.java
+++ src/main/java/com/hzokbe/ongaku/config/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
@@ -44,9 +45,12 @@ public class SecurityConfig {
                 r -> r
                     .requestMatchers(HttpMethod.POST, "/sign-up")
                     .permitAll()
+                    .requestMatchers(HttpMethod.POST, "/sign-in")
+                    .permitAll()
                     .anyRequest()
                     .denyAll()
             )
+            .httpBasic(Customizer.withDefaults())
             .build();
     }
 
