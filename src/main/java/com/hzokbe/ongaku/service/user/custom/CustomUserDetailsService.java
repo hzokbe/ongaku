3d836cd864748057d643fe3d8e7aed9527acc7d1
hzokbe <hzokbe@gmail.com>
2025-07-03 19:23:00 -0300

    feat(service): implement UserDetailsService

--- src/main/java/com/hzokbe/ongaku/service/user/custom/CustomUserDetailsService.java
+++ src/main/java/com/hzokbe/ongaku/service/user/custom/CustomUserDetailsService.java
@@ -1,14 +1,36 @@
 package com.hzokbe.ongaku.service.user.custom;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.hzokbe.ongaku.repository.user.UserRepository;
 
 @Service
-public class CustomUserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository repository;
 
     public CustomUserDetailsService(UserRepository repository) {
         this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var optionalUser = repository.findByUsername(username);
+
+        if (optionalUser.isEmpty()) {
+            throw new UsernameNotFoundException("username not found");
+        }
+
+        var user = optionalUser.get();
+
+        return User
+            .builder()
+            .username(username)
+            .password(user.getPasswordHash())
+            .roles(user.getRole().name())
+            .build();
     }
 }
