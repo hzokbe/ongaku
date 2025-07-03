8ee8bc43f2393fa9565616cc89efad1792f168bd
hzokbe <hzokbe@gmail.com>
2025-07-03 18:39:40 -0300

    feat(model): add JwtResponse class

---
+++ src/main/java/com/hzokbe/ongaku/model/jwt/response/JwtResponse.java
@@ -0,0 +1,17 @@
+package com.hzokbe.ongaku.model.jwt.response;
+
+public class JwtResponse {
+    private String jwt;
+
+    public JwtResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+}
