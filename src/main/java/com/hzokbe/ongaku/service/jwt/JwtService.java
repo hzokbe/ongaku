3a1ae79ad50fa2cc28fee5a8713b3f405eae2307
hzokbe <hzokbe@gmail.com>
2025-07-03 19:12:07 -0300

    feat(service): add generateJwt method

--- src/main/java/com/hzokbe/ongaku/service/jwt/JwtService.java
+++ src/main/java/com/hzokbe/ongaku/service/jwt/JwtService.java
@@ -1,7 +1,14 @@
 package com.hzokbe.ongaku.service.jwt;
 
+import java.time.Instant;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
+
+import com.hzokbe.ongaku.model.jwt.response.JwtResponse;
 
 @Service
 public class JwtService {
@@ -9,5 +16,22 @@ public class JwtService {
 
     public JwtService(JwtEncoder encoder) {
         this.encoder = encoder;
+    }
+
+    public JwtResponse generateJwt(Authentication authentication) {
+        var issuedAt = Instant.now();
+
+        var expiresAt = issuedAt.plusSeconds(3600);
+
+        var claims = JwtClaimsSet
+            .builder()
+            .issuer("ongaku")
+            .issuedAt(issuedAt)
+            .expiresAt(expiresAt)
+            .subject(authentication.getName())
+            .claim("roles", authentication.getAuthorities().stream().map(a -> a.getAuthority()).toList())
+            .build();
+
+        return new JwtResponse(encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue());
     }
 }
