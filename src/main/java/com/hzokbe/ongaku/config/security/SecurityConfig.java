8c8729f937a1925e10b41ae5ed4c5729e6174a93
hzokbe <hzokbe@gmail.com>
2025-07-04 19:41:48 -0300

    feat(config): add getJwtAuthenticationConverter method

--- src/main/java/com/hzokbe/ongaku/config/security/SecurityConfig.java
+++ src/main/java/com/hzokbe/ongaku/config/security/SecurityConfig.java
@@ -2,20 +2,28 @@ package com.hzokbe.ongaku.config.security;
 
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.nimbusds.jose.jwk.JWKSet;
@@ -69,5 +77,18 @@ public class SecurityConfig {
     @Bean
     public JwtDecoder getJwtDecoder() {
         return NimbusJwtDecoder.withPublicKey(rsaPublicKey).build();
+    }
+
+    public JwtAuthenticationConverter getJwtAuthenticationConverter() {
+        var converter = new JwtAuthenticationConverter();
+
+        converter.setJwtGrantedAuthoritiesConverter(
+            jwt -> jwt.getClaimAsStringList("roles")
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.toUpperCase()))
+                .collect(Collectors.toList())
+        );
+
+        return converter;
     }
 }
