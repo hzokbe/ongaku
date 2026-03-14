ff26de45063fb2255cf3c5bc5ebea4bd237faed3
hzokbe <hzokbe@gmail.com>
2026-03-14 13:41:43 -0300

    feat(routes): add /sign-in route

--- src/main/java/com/hzokbe/ongaku/Main.java
+++ src/main/java/com/hzokbe/ongaku/Main.java
@@ -1,9 +1,11 @@
+import com.hzokbe.ongaku.dto.auth.SignInRequestDTO;
 import com.hzokbe.ongaku.dto.auth.SignUpRequestDTO;
 import com.hzokbe.ongaku.service.auth.AuthService;
 import io.javalin.Javalin;
 import io.javalin.config.JavalinConfig;
 
 import static io.javalin.http.HttpStatus.CREATED;
+import static io.javalin.http.HttpStatus.OK;
 
 AuthService authService = new AuthService();
 
@@ -18,5 +20,11 @@ void registerRoutes(JavalinConfig config) {
         var dto = context.bodyAsClass(SignUpRequestDTO.class);
 
         context.status(CREATED).json(authService.signUp(dto));
+    });
+
+    config.routes.post("/sign-in", context -> {
+        var dto = context.bodyAsClass(SignInRequestDTO.class);
+
+        context.status(OK).json(authService.signIn(dto));
     });
 }
