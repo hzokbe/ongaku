590e459ba33ecf77270ef901447d32cecd3b2c0b
hzokbe <hzokbe@gmail.com>
2026-03-14 21:28:05 -0300

    feat(service): add song service

---
+++ src/main/java/com/hzokbe/ongaku/service/song/SongService.java
@@ -0,0 +1,7 @@
+package com.hzokbe.ongaku.service.song;
+
+import com.hzokbe.ongaku.repository.song.SongRepository;
+
+public class SongService {
+    private final SongRepository repository = new SongRepository();
+}
